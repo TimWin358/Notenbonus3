3ca18478a49b8a12e7215e1bdb070f89094ffd1c
Jennifer <ge96cor@mytum.de>
2021-02-13 17:38:51 +0100

    Collectables

---
+++ Platformer/Assets/Scripts/CollectCollectable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCollectable : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        ScoringSystem.theScore += 50;
+        Destroy(gameObject);
+    }
+}
